4973c4f4e250d5ff699aa6dd294616f199331169
Harm Behrens <hb@netzfisch.de>
2012-09-20 08:08:29 +0200

    change files

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  skip_before_filter :require_login, :only => [:new, :create]
+  skip_before_filter :require_login
   # GET /users
   # GET /users.json
   def index
