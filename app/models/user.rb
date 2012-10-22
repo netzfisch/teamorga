08239e7e722c9723591c10a061274fdadc16b725
Harm Behrens <hb@netzfisch.de>
2012-10-22 22:16:38 +0200

    killed validation of t-shirt

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,14 +4,15 @@ class User < ActiveRecord::Base
   has_many :comments, :dependent => :delete_all
 
   accepts_nested_attributes_for :participations #, :allow_destroy => true
-  
+
   validates_presence_of :email, :password, :on => :create
   validates_uniqueness_of :email, :case_sensitive => false
   has_secure_password
-  
-  validates_presence_of :name, :phone, :on => :update  
-  validates_uniqueness_of :name, :phone, :shirt_number #, :on => :update 
+
+  validates_presence_of :name, :phone, :on => :update
+  validates_uniqueness_of :name, :phone, :on => :update
   validates :shirt_number, :length => { :maximum => 2 }
 
   attr_accessible :name, :email, :phone, :birthday, :shirt_number, :password, :password_confirmation, :recurrence_ids
 end
+
