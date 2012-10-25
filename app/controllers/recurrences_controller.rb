3d4acb4baecbaac9419175e7c27e7d27708f997a
Harm Behrens <hb@netzfisch.de>
2012-10-25 21:25:22 +0200

    force ssl, Berlin timezone, admin function

--- app/controllers/recurrences_controller.rb
+++ app/controllers/recurrences_controller.rb
@@ -1,18 +1,18 @@
 class RecurrencesController < ApplicationController
-  layout 'with_sidebar', :only => :index 
+  layout 'with_sidebar', :only => :index
 
   # GET /recurrences
   # GET /recurrences.json
   def index
     @users = User.all(:order => :name)
     @recurrences = Recurrence.paginate(page: params[:page], per_page: 5, :order => :scheduled_to,
-      :conditions => { :scheduled_to => (Date.today)..(Date.today + 1.year) } )    
+      :conditions => { :scheduled_to => (Date.today)..(Date.today + 1.year) } )
     # before
-    # @recurrences = Recurrence.all(:order => :scheduled_to, 
+    # @recurrences = Recurrence.all(:order => :scheduled_to,
     # :conditions => { :scheduled_to => (Date.today)..(Date.today + 5.weeks) } )
-  
-    @comments = Comment.all(:order => 'created_at DESC', :limit => 5)  
-    
+
+    @comments = Comment.all(:order => 'comment.base_date DESC', :limit => 5)
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @recurrences }
@@ -25,7 +25,7 @@ class RecurrencesController < ApplicationController
     @recurrence = Recurrence.find(params[:id])
       #  @users = User.all
     #@user = User.find(params[:id])
-    
+
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @recurrence }
@@ -91,5 +91,6 @@ class RecurrencesController < ApplicationController
       format.json { head :no_content }
     end
   end
-  
+
 end
+
