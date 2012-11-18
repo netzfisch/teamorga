1ce664a17cf61a1767a59508d0814bc03f1022a1
Harm Behrens <hb@netzfisch.de>
2012-11-18 20:28:00 +0100

    finalized user login specification

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,10 +3,10 @@ require 'spec_helper'
 describe "home page" do
 
   it "show home page after successful login" do
-    user = User.create!(:email => "jdoe", :password => "secret", :name => "jdoe")
-    # change to factory_girl
-    # user = FactoryGirl.create(:user, :username => "jdoe", :password => "secret", :name => "jdoe")
-    visit "/login"
+    user = User.create!(:name => "jdoe", :email => "jdoe", :password => "secret")
+    # why should I change to factory_girl:
+    # user = FactoryGirl.create(:user, :name => "jdoe", :username => "jdoe", :password => "secret")
+    visit login_path # alternatively take the direct path "/login"
     fill_in "Email", :with => "jdoe"
     fill_in "Password", :with => "secret"
     click_button "Log in"
