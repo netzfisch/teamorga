6583548e2ff7c57620533f829110d4c8273ae533
Harm Behrens <hb@netzfisch.de>
2012-11-18 20:26:24 +0100

    finalized specs for index action for reccurence controller

--- spec/controllers/recurrences_controller_spec.rb
+++ spec/controllers/recurrences_controller_spec.rb
@@ -2,44 +2,48 @@ require 'spec_helper'
 
 describe RecurrencesController, "GET index" do
 
-# not working, because:
-# application_controller filter: "before_filter :require_login"
-# recurrences_controller method: "@recurrences = Recurrence.find :all"
+# REFACTOR, beautyfy and maybe change to shoulda gem, see http://codereview.stackexchange.com/questions/505/how-to-effectively-unit-test-a-controller-in-ruby-on-rails-please-critique-a-sa
+
+    let(:recurrence) { [mock_model(Recurrence)] }
 
     before :each do
-# better as helper method: sign_in user
-user = User.create!(:email => "jdoe", :password => "secret", :name => "jdoe")
-visit "/login"
-fill_in "Email", :with => "jdoe"
-fill_in "Password", :with => "secret"
-click_button "Log in"
-      @recurrence = mock_model(Recurrence)
-      Recurrence.stub!(:find).with(:all).and_return([@recurrence])
-      #Recurrence.stub!(:total_pages).and_return(0)
+      user = User.create!(:email => "jdoe", :password => "secret", :name => "jdoe")
+      request.session = { :user_id => user.id } # alternatively "session[:user_id] = user.id
+      #@recurrence = mock_model(Recurrence)
+
+      Recurrence.stub_chain(:visible, :paginate).and_return([@recurrence])
+      # with "find.all": Recurrence.stub!(:find).with(:all).and_return([@recurrence])
     end
 
-    def do_get
+    it "should assign an array of recurrences" do
       get :index
+      assigns[:recurrences].should be_instance_of(Array)
+      expect(assigns(:recurrences)).to eq([@recurrence])
     end
 
     it "should be successfull" do
-      do_get
+      get :index
       response.should be_success
     end
 
-    it "should assign recurrences" do
-      do_get
-      assigns[:recurrences].should be_instance_of(Array)
-    end
-
-    it "should call the find method of the recurrence class" do
-      Recurrence.should_receive(:find).with(:all).and_return(@recurrence)
-      do_get
-    end
-
     it "should render the index template" do
-      do_get
+      get :index
       response.should render_template("index")
+    end
+
+    it "should call the paginate method of the recurrence class" do
+      Recurrence.visible.should_receive(:paginate).and_return(@recurrence)
+      get :index
+    end
+
+    it "should defaults showing 5 results per page" do
+      Recurrence.visible.should_receive(:paginate).with(:page => nil, :per_page => 5).and_return(@recurrence)
+      get :index
+    end
+
+    it "should pass on the page number to will_paginate" do
+      Recurrence.visible.should_receive(:paginate).with(:page => '3', :per_page => 5).and_return(@recurrence)
+      get :index, 'page' => '3'
     end
 
 end
