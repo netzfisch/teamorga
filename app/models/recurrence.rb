f852736f6928f5abf69b2c3daa59f24bba81661e
Harm Behrens <hb@netzfisch.de>
2012-12-15 15:22:29 +0100

    moved ordering to default_scope

--- app/models/recurrence.rb
+++ app/models/recurrence.rb
@@ -1,18 +1,18 @@
 class Recurrence < ActiveRecord::Base
   belongs_to :event
   has_many :participations, :include => :user, :dependent => :delete_all
-  has_many :users, :through => :participations #, :include => :user
+  has_many :users, :through => :participations
   has_many :comments, :dependent => :delete_all #, :order => 'created_at DESC'
 
   accepts_nested_attributes_for :participations #, :allow_destroy => true
   attr_accessible :scheduled_to, :event_id, :user_id, :user_ids
 
-  scope :current, lambda { (where("scheduled_to >= ?", Time.zone.now)).order("scheduled_to") }
-  scope :visible, current.order("scheduled_to") # move to schema! standard sort!
+  default_scope order("scheduled_to")
+  scope :current, lambda { where("scheduled_to >= ?", Time.zone.now) }
 
-  # TODO combinig visible & paginate with scope
-  def self.visible_paginate(page, per_page)
-    recurrences = visible.paginate(page, per_page)
+  # TODO combinig current & paginate with scope
+  def self.current_paginate(page, per_page)
+    recurrences = current.paginate(page, per_page)
   end
 end
 
