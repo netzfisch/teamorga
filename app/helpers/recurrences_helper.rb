190688020eae10f1f5ba0022dc716fc55005e51d
Harm Behrens <hb@netzfisch.de>
2012-12-29 13:41:36 +0100

    removed old accept-, refuse-buttons

--- app/helpers/recurrences_helper.rb
+++ app/helpers/recurrences_helper.rb
@@ -4,16 +4,16 @@ module RecurrencesHelper
     if current_user.responded?(recurrence)
 
       if current_user.responded_at(recurrence).status == true
-        button_to "doch noch Absagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put
+        button_to "doch noch Absagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put, :class => 'btn btn-mini btn-danger'
       elsif current_user.responded_at(recurrence).status == false
-        button_to "doch noch Zusagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put
+        button_to "doch noch Zusagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put, :class => 'btn btn-mini btn-success'
       end
 
     elsif overrule == 'accept'
-      button_to "Zusagen", create_status_participation_path(recurrence, status: true)
+      button_to "Zusagen", create_status_participation_path(recurrence, status: true), :class => 'btn btn-mini btn-success'
 
     elsif overrule == 'refuse'
-      button_to "Absagen", create_status_participation_path(recurrence, status: false)
+      button_to "Absagen", create_status_participation_path(recurrence, status: false), :class => 'btn btn-mini btn-danger'
 
     end
   end
