128991d1f162ef89f295c36007ff07a6a96634f2
Harm Behrens <hb@netzfisch.de>
2012-12-31 16:18:57 +0100

    created migration data with 'status: true'

--- db/migrate/20121222225239_add_status_to_participations.rb
+++ db/migrate/20121222225239_add_status_to_participations.rb
@@ -1,6 +1,23 @@
 class AddStatusToParticipations < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :participations, :status, :boolean, default: false
+
+    Participation.update_all(status: true)
+    # 2012-12-29-hb! - alternatively MANUAL migration for new column 'participation.status':
+    # - every existing entry in the participation table, represents acceptance
+    # - so every existing participation.status need to be set to 'true'
+    #
+    # $ heroku run rails console
+    # participations = Participation.find(:all)
+    # participations.each { |p| p.update_attributes(status: true); p.save }
+    #
+    # shorter:
+    # $ heroku run rails console
+    # Participation.find.each { |p| p.update_attribute(status: true); p.save }
+  end
+
+  def self.down
+    remove_column :participations, :status
   end
 end
 
