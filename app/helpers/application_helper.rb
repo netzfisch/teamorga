0d504eaa7c40c89aa13bf6aba2ace336c631b848
Harm Behrens <hb@netzfisch.de>
2013-01-04 14:10:12 +0100

    added img-\/title-tag to email_link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,11 +2,12 @@ module ApplicationHelper
 
   def email_link(users, recurrence = nil)
     if users.count != nil
-      mail_to(users.map(&:email).join(", "), "email2all !", html_options = {
-        subject: "#{recurrence.event.category} am #{recurrence.scheduled_to.strftime("%e. %B %Y")}",
-        body: "Hey,\n
-               wir brauchen mehr Leute ..."
-        })
+        mail_to(users.map(&:email).join(", "), name = "", html_options = {
+          class: "icon-envelope",
+          title: "eMail an aller dieser Gruppe versenden!",
+          subject: "#{recurrence.event.category} am #{recurrence.scheduled_to.strftime("%e. %B %Y")}",
+          body: "Hey,\nwir brauchen mehr Leute ..."
+          })
     end
   end
 
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -8,16 +8,17 @@ let(:recurrence) { FactoryGirl.create(:recurrence) }
 
   describe "#admin_link"
 
+  describe "#comment_link"
+
   describe "#email_link" do
     it "should generate the link" do
-      expect(helper.email_link([user], recurrence)).to match(Regexp.escape(/a href="mailto:john@doe.com?(.*)body=(.*)subject=(.*)">email !</a>/i)
+      expect(helper.email_link([user], recurrence)).to eq('<a class="icon-envelope" href="mailto:john@doe.com?body=Hey%2C%0Awir%20brauchen%20mehr%20Leute%20...&amp;subject=Training%20am%2011.%20January%202013" title="eMail an aller dieser Gruppe versenden!"></a>')
+
+      # match(Regexp.escape(/a href="mailto:john@doe.com?(.*)body=(.*)subject=(.*)">email !</a>/i)
     end
   end
 
-<i class="icon-envelope"></i>
-
-
-  describe "#google_link"
+  describe "2x #google_link"
 
 end
 
