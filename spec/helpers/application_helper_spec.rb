15728bec35782c8d8eeb23b35955fdd2a8b0b3ca
Harm Behrens <hb@netzfisch.de>
2013-01-06 00:49:47 +0100

    changed mailto-link matching to regex

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -6,19 +6,19 @@ describe ApplicationHelper do
 let(:user) { FactoryGirl.create(:user, email: "john@doe.com") }
 let(:recurrence) { FactoryGirl.create(:recurrence) }
 
-  describe "#admin_link"
+  it "#admin_link"
 
-  describe "#comment_link"
+  it "#comment_link"
 
   describe "#email_link" do
     it "should generate the link" do
-      expect(helper.email_link([user], recurrence)).to eq('<a class="icon-envelope" href="mailto:john@doe.com?body=Hey%2C%0Awir%20brauchen%20mehr%20Leute%20...&amp;subject=Training%20am%2011.%20January%202013" title="eMail an aller dieser Gruppe versenden!"></a>')
-
-      # match(Regexp.escape(/a href="mailto:john@doe.com?(.*)body=(.*)subject=(.*)">email !</a>/i)
+      helper.email_link([user], recurrence).should match(/\<a class="icon-envelope" href="mailto:john@doe.com?(.*)body=(.*)subject=(.*)" title="(.*)"\>\<\/a\>/)
     end
   end
 
-  describe "2x #google_link"
+  it "#format_date"
+
+  it "#2x #google_link"
 
 end
 
