d767a06f885f39f1493a1241e910b1192117e68c
Harm Behrens <hb@netzfisch.de>
2013-01-07 00:15:56 +0100

    logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Teamorga::Application.routes.draw do
+
   get "logout" => "sessions#destroy", :as => "logout"
   get "login" => "sessions#new", :as => "login"
   get "signup" => "users#new", :as => "signup"
