3da17a9e6b87dc35edc16e65ca01481c266d7423
Harm Behrens <hb@netzfisch.de>
2013-01-07 22:17:20 +0100

    replaced if-clause with ternary operator

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,19 +1,14 @@
 module ApplicationHelper
 
   def email_link(users, recurrence = nil)
-    if users.count != nil
-        mail_to(users.map(&:email).join(", "), name = "", html_options = {
-          class: "icon-envelope",
-          title: "eMail an Spieler dieser Gruppe versenden!",
-          subject: "#{ unless recurrence == nil
-                         recurrence.event.category
-                       end }"+" am "+
-                    "#{ unless recurrence == nil
-                         recurrence.scheduled_to.strftime("%e. %B %Y")
-                       end }",
-          body: "Hey,\nwir brauchen mehr Bier/Leute ..."
-          })
-    end
+    unless users == []
+      mail_to(users.map(&:email).join(", "), name = "", html_options = {
+        class: "icon-envelope",
+        title: "eMail an Spieler dieser Gruppe versenden!",
+        subject: "#{ recurrence ? recurrence.event.category+' am '+recurrence.scheduled_to.strftime("%e. %B %Y") : "Rundmail 2all" }",
+        body: "Hey,\nwir brauchen mehr Bier/Leute ..."
+        })
+     end
   end
 
 end
