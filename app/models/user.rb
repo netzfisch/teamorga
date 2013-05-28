5eb468796b6b1d656e3f85dccf38b99d31602cbe
harm <hb@netzfisch.de>
2013-05-28 14:54:45 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,7 +19,7 @@ class User < ActiveRecord::Base
 
   default_scope order("name")
   scope :licence, where("shirt_number IS NOT NULL") # count(:shirt_number) or shirt_number.exist?
-  scope :upcoming_birthdays, lambda { where("User.next_birthday between ? and ?", Date.today - 1.week, Date.today + 2.weeks) }
+  #scope :upcoming_birthdays, lambda { where("User.next_birthday between ? and ?", Date.today - 1.week, Date.today + 2.weeks) }
 
   def responded?(recurrence)
     participations.any? { | participation | participation.recurrence_id == recurrence.id }
@@ -38,6 +38,10 @@ class User < ActiveRecord::Base
     birthday += 1.year if birthday < Date.today 
   end
 
-  
+  def upcoming_birthdays
+    # birthdays = User.find(:all).
+    where("next_birthday between ? and ?", Date.today - 1.week, Date.today + 2.weeks) }
+    # birthdays = User.find(:all, :select => 'id, birthday').sort_by(&:next_birthday).first(5)
+  end
 end
 
