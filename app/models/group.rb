07dc137770ce3b13b3d958e0306a178bc852b747
Harm Behrens <hb@netzfisch.de>
2013-10-27 16:23:47 +0100

    add spec for validation of group name presence

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,3 +1,5 @@
 class Group < ActiveRecord::Base
   attr_accessible :logo_url, :name, :private_information, :public_information
+
+  validates_presence_of :name
 end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,10 @@
 require 'spec_helper'
 
 describe Group do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without a name" do
+    invalid_group = Group.create(name: nil)
+    invalid_group.should_not be_valid
+    invalid_group.should have(1).errors_on(:name)
+    invalid_group.errors[:name].should include("can't be blank")
+  end
 end
