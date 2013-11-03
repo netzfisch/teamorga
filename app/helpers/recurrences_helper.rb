695b336a5f06a03a5b2c90ace375a6e3e2bb0638
Harm Behrens <hb@netzfisch.de>
2013-11-03 19:59:54 +0100

    test view changes: buttons, icons, panels

--- app/helpers/recurrences_helper.rb
+++ app/helpers/recurrences_helper.rb
@@ -4,18 +4,17 @@ module RecurrencesHelper
     if current_user.responded?(recurrence)
 
       if current_user.responded_at(recurrence).status == true
-        button_to "doch noch Absagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put, :class => 'btn btn-mini btn-danger'
+        button_to "doch noch Absagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put, :class => 'btn btn-xs btn-danger'
       elsif current_user.responded_at(recurrence).status == false
-        button_to "doch noch Zusagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put, :class => 'btn btn-mini btn-success'
+        button_to "doch noch Zusagen", toggle_status_participation_path(current_user.responded_at(recurrence)), method: :put, :class => 'btn btn-xs btn-success'
       end
 
     elsif overrule == 'accept'
-      button_to "Zusagen", create_status_participation_path(recurrence, status: true), :class => 'btn btn-mini btn-success'
+      button_to "Zusagen", create_status_participation_path(recurrence, status: true), :class => 'btn btn-xs btn-success'
 
     elsif overrule == 'refuse'
-      button_to "Absagen", create_status_participation_path(recurrence, status: false), :class => 'btn btn-mini btn-danger'
+      button_to "Absagen", create_status_participation_path(recurrence, status: false), :class => 'btn btn-xs btn-danger'
 
     end
   end
 end
-
