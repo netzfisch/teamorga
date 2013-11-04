3d6f68fb24dfeef24199f613290278dd7cb5187d
Harm Behrens <hb@netzfisch.de>
2013-11-04 22:16:03 +0100

    change mainly forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -37,10 +37,10 @@ module ApplicationHelper
   def email_link(users, recurrence = nil)
     unless users == []
       mail_to(users.map(&:email).join(", "), name = "", html_options = {
-        class: "icon-envelope",
+        class: "glyphicon glyphicon-envelope",
         title: "eMail an Spieler dieser Gruppe versenden!",
         subject: "#{ recurrence ? recurrence.event.category+' am '+recurrence.scheduled_to.strftime("%e. %B %Y") : "Rundmail 2all" }",
-        body: "Hey,\nwir brauchen mehr Bier/Leute ..."
+        body: "Hey,\nwir brauchen mehr ..."
         })
      end
   end
